aeefe98157257d44a76b38cb484fd9de151c7e99
ishankoul <ishan@blockgemini.com>
2018-10-24 12:16:59 +0400

    service registry

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,23 @@
 'use strict';
-
+const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
-server.listen(3010);
+server.listen();
 
 server.on('listening', () => {
     console.log(`IRIS-Time is listening on ${server.address().port} in ${service.get('env')}`);
+    const announce = () => {
+        request.put(`http://localhost:3000/service/time/${server.address().port}`, (err, response) => {
+            if(err){
+                console.log(err);
+                console.log("Error connecting to iris");
+                return;
+            }
+            console.log(response.body);
+        })
+    }
+    announce();
+    setInterval(announce, 15 * 1000)
 })
